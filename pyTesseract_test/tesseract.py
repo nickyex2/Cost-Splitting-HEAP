46a413e1410348a01bd91080856c2f5e5f8c8b24
Nicholas Goh <zellaex128@gmail.com>
2022-07-10 21:07:26 +0800

    Function and few more photos added

--- pyTesseract_test/tesseract.py
+++ pyTesseract_test/tesseract.py
@@ -4,12 +4,11 @@ import pytesseract
 
 # images that can be recognised needs to be clearly printed with the total clearly seen on the receipt, faded receipts cannot be accepted
 
-img = cv2.imread('receipt2.jpg',0) # change to variable when this is converted into a function // 0 = greyscale 
+img = cv2.imread('D:\\Github\\Cost-Splitting-HEAP\\pyTesseract_test\\IMG_16.jpeg',0) # change to variable when this is converted into a function // 0 = greyscale 
 
 custom_config = r'--oem 3 --psm 6'
 output = pytesseract.image_to_data(img, config=custom_config, output_type='dict')
 true_output = output['text']
-
 #placing everything into an array
 placeholder = []
 temp = ''
@@ -24,6 +23,7 @@ for item in true_output:
         else:
             temp += " " + item
 
+print(placeholder)
 #since main total is always at the back, therefore run the array from the back to find the first instance of total
 #haven think of formatting of receipts yet as some receipts has huge spacing between total and value
 placeholder.reverse()
@@ -32,7 +32,8 @@ num = 0
 total = ''
 while(not finder):
     tempp = placeholder[num].lower()
-    if 'total' in tempp:
+    tempp = tempp.split()
+    if ('total' in tempp or "total:" in tempp) and len(tempp) <= 3:
         finder = True
         total = placeholder[num]
     num += 1
@@ -41,6 +42,8 @@ temppp = ''
 for char in total:
     if char.isnumeric() or char == '.':
         temppp += char
+    elif char.lower() in 'total' and temppp != '':
+        temppp = ''
 output_dict['Total'] = float(temppp)
 # to be returned as a dict variable {string : float}
 print(output_dict)
